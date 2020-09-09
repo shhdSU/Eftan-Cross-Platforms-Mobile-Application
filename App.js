9b9a8fa7d78abb9a7bebf4a92aaad63f886088b3
SarahAlgwaiz <susu_5_99@hotmail.com>
2020-09-09 19:53:25 +0300

    Firebase changes

--- App.js
+++ App.js
@@ -2,20 +2,41 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import * as firebase from "firebase";
+
+//Initialize Firebase
 const firebaseConfig = {
-an  apiKey: "AIzaSyDtSPt8lV9R-dkmkCXxCI0xNadkq9aAtUk",
-  authDomain: "<your-auth-domain>",
-  databaseURL: "<your-database-url>",
-  storageBucket: "<your-storage-bucket>",
+  apiKey: "AIzaSyDtSPt8lV9R-dkmkCXxCI0xNadkq9aAtUk",
+  authDomain: "eftan2020.firebaseapp.com",
+  databaseURL: "https://eftan2020.firebaseio.com",
+  projectId: "eftan2020",
+  storageBucket: "eftan2020.appspot.com",
 };
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+firebase.initializeApp(firebaseConfig);
+import {
+  Container,
+  Content,
+  Header,
+  Item,
+  Form,
+  Input,
+  Button,
+  Label,
+} from "native-base";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Container style= {styles.container}>
+      <Form>
+        <Item>
+          <Label>Email</Label>
+          <Input 
+          autoCorrect = {false}
+          autoCapitalize = "none"
+          />
+        </Item>
+      </Form>
+    </Container> 
+     );
 }
 
 const styles = StyleSheet.create({

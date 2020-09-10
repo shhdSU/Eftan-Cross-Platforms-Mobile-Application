d63bfe7e77939e40dda18c9a3f87b6aaeab2f222
SarahAlgwaiz <susu_5_99@hotmail.com>
2020-09-10 16:23:06 +0300

    Adding screens
    Login-Registration-Home Screen added + styles.js

--- App.js
+++ App.js
@@ -1,49 +1,36 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import * as firebase from "firebase";
-
-//Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyDtSPt8lV9R-dkmkCXxCI0xNadkq9aAtUk",
-  authDomain: "eftan2020.firebaseapp.com",
-  databaseURL: "https://eftan2020.firebaseio.com",
-  projectId: "eftan2020",
-  storageBucket: "eftan2020.appspot.com",
-};
-
-firebase.initializeApp(firebaseConfig);
-import {
-  Container,
-  Content,
-  Header,
-  Item,
-  Form,
-  Input,
-  Button,
-  Label,
-} from "native-base";
-export default function App() {
-  return (
-    <Container style= {styles.container}>
-      <Form>
-        <Item>
-          <Label>Email</Label>
-          <Input 
-          autoCorrect = {false}
-          autoCapitalize = "none"
-          />
-        </Item>
-      </Form>
-    </Container> 
-     );
+import "react-native-gesture-handler";
+import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { LoginScreen, HomeScreen, RegistrationScreen } from "./src/screens";
+import { decode, encode } from "base-64";
+if (!global.btoa) {
+  global.btoa = encode;
+}
+if (!global.atob) {
+  global.atob = decode;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const Stack = createStackNavigator();
+
+export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {user ? (
+          <Stack.Screen name="Home">
+            {(props) => <HomeScreen {...props} extraData={user} />}
+          </Stack.Screen>
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Registration" component={RegistrationScreen} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}

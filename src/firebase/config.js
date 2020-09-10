00644f2e5c419c4c25fadc7f8128c3c118365182
SarahAlgwaiz <susu_5_99@hotmail.com>
2020-09-10 16:25:38 +0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,14 @@ import "@firebase/auth";
 import "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY_HERE_AIzaSyAOWH",
-  authDomain: "your-auth-domain-b1234.firebaseapp.com",
-  databaseURL: "https://your-database-name.firebaseio.com",
-  projectId: "your-project-id-1234",
-  storageBucket: "your-project-id-1234.appspot.com",
-  messagingSenderId: "12345-insert-yourse",
-  appId: "insert yours: 1:1234:web:ee873bd1234c0deb7eba61ce",
+    apiKey: "AIzaSyDtSPt8lV9R-dkmkCXxCI0xNadkq9aAtUk",
+    authDomain: "eftan2020.firebaseapp.com",
+    databaseURL: "https://eftan2020.firebaseio.com",
+    projectId: "eftan2020",
+    storageBucket: "eftan2020.appspot.com",
+     messagingSenderId: "782913922565",
+     appId: "1:782913922565:web:856c5c1c336cf2e0415e6c",
+     measurementId: "G-J6S9B6L3WK",
 };
 
 if (!firebase.apps.length) {

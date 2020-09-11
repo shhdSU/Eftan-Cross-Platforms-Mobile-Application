c838e171d7994d2304ea736f2e3cb8f342eb2fb2
SarahAlgwaiz <susu_5_99@hotmail.com>
2020-09-11 15:23:32 +0300

    app.js and login.js

--- App.js
+++ App.js
@@ -1,4 +1,8 @@
 import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./components/login";
+import Signup from "./components/signup";
 import React from "react";
 import {
   StyleSheet,
@@ -11,7 +15,7 @@ import {
 } from "react-native";
 import firebase from "../Eftan/database/firebase";
 
-export default class App extends React.Component {
+class Screens extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -28,7 +32,7 @@ export default class App extends React.Component {
     this.setState(state);
   };
 
-  registerUser = () => {
+  registerUser = ({ navigation }) => {
     if (this.state.email === "" && this.state.password === "") {
       Alert.alert("Enter details to signup!");
     } else {
@@ -59,7 +63,7 @@ export default class App extends React.Component {
             email: "",
             password: "",
           });
-          this.props.navigation.navigate("Login");
+          navigation.navigate("Login");
         })
         .catch((error) => this.setState({ errorMessage: error.message }));
     }
@@ -73,43 +77,59 @@ export default class App extends React.Component {
         </View>
       );
     }
-    return (
-      <View style={styles.container}>
-        <TextInput
-          style={styles.inputStyle}
-          placeholder="Name"
-          value={this.state.displayName}
-          onChangeText={(val) => this.updateInputVal(val, "displayName")}
-        />
-        <TextInput
-          style={styles.inputStyle}
-          placeholder="Email"
-          value={this.state.email}
-          onChangeText={(val) => this.updateInputVal(val, "email")}
-        />
-        <TextInput
-          style={styles.inputStyle}
-          placeholder="Password"
-          value={this.state.password}
-          onChangeText={(val) => this.updateInputVal(val, "password")}
-          maxLength={15}
-          secureTextEntry={true}
-        />
-        <Button
-          color="#3740FE"
-          title="Signup"
-          onPress={() => this.registerUser()}
-        />
-
-        <Text
-          style={styles.loginText}
-          onPress={() => this.props.navigation.navigate("Login")}
-        >
-          Already Registered? Click here to login
-        </Text>
-      </View>
-    );
   }
+}
+function SignupScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Name"
+        value={this.state.displayName}
+        onChangeText={(val) => this.updateInputVal(val, "displayName")}
+      />
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Email"
+        value={this.state.email}
+        onChangeText={(val) => this.updateInputVal(val, "email")}
+      />
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Password"
+        value={this.state.password}
+        onChangeText={(val) => this.updateInputVal(val, "password")}
+        maxLength={15}
+        secureTextEntry={true}
+      />
+      <Button
+        color="#3740FE"
+        title="Signup"
+        onPress={() => this.registerUser()}
+      />
+
+      <Text
+        style={styles.loginText}
+        onPress={() => navigation.navigate("Login")}
+      >
+        Already Registered? Click here to login
+      </Text>
+    </View>
+  );
+}
+
+function LoginScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+  
+  );
 }
 
 const styles = StyleSheet.create({

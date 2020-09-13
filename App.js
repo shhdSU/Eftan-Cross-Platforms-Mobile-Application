40fe93992ebc061736fe335bbb2a63f38e4fb308
lujain <lujainalmuhaitheef@gmail.com>
2020-09-13 21:35:57 +0300

    edite navigate

--- App.js
+++ App.js
@@ -3,10 +3,13 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import LoginScreen from "./components/login";
 import SignupScreen from "./components/signup";
+import GalleryScreen from "./components/gallery";
+
 import firebase from "../Eftan/database/firebase";
 
 const Navigation = createStackNavigator({
   Signup: { screen: SignupScreen },
   Login: { screen: LoginScreen },
+  gallery: { screen: GalleryScreen },
 });
 export default createAppContainer(Navigation);
---
+++ components/gallery.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import firebase from "../database/firebase";
+// view its like div in web :)
+//class LoadingScreen
+export default class HomeScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      isLoading: false,
+      userType: 1,
+    };
+  }
+
+  /*componentDidMount() {
+    const { email, displayName } = firebase.auth().currentUser;
+    this.setState({ email, displayName });
+  }
+  */
+  signOutUser = () => {
+    const { email, password } = this.state;
+    firebase.auth().signOut();
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>مرحبا {this.state.email} !</Text>
+        <TouchableOpacity style={{ marginTop: 32 }} onPress={this.signOutUser}>
+          <Text>تسجيل الخروج</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+//Style sheet
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});

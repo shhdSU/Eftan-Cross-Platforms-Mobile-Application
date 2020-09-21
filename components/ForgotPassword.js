11582f1c61a207dd965710c3b7c92b3d765cb79a
SarahAlgwaiz <susu_5_99@hotmail.com>
2020-09-21 12:29:00 +0300

    alert fix

--- components/ForgotPassword.js
+++ components/ForgotPassword.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import { Text, View, TextInput, StyleSheet } from "react-native";
+import { Text, View, Alert, TextInput, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import firebase from "../database/firebase";
 import {

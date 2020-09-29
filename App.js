37183d0b7b7a7d6485d9105aaf9f8cf728568604
Shahad Alkaltham <48352913+shhdSU@users.noreply.github.com>
2020-09-29 22:48:07 +0300

    I dont Know

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
-
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import LoginScreen from "./components/login";
 import SignupScreen from "./components/signup";
 import GalleryScreen from "./components/gallery";
-import DprofileScreen from "./components/DProfile"; // Access by Client 
+import DprofileScreen from "./components/DProfile"; // Access by Client
 import privacyPolicyScreen from "./components/privacyPolicy";
 import ForgotPassword from "./components/ForgotPassword";
 import DesignerProfile from "./components/designerProfile"; // Access by Designer
 import RequestScreen from "./components/requestPage";
 
-
-
 const Navigation = createStackNavigator(
   {
+    "صفحة الطلب": { screen: RequestScreen },
     "صفحة الدخول": { screen: LoginScreen },
     "صفحة التسجيل": { screen: SignupScreen },
     "معرض المصمم": { screen: GalleryScreen },
@@ -21,8 +19,6 @@ const Navigation = createStackNavigator(
     "سياسة الخصوصية": { screen: privacyPolicyScreen },
     "نسيت كلمة السر!": { screen: ForgotPassword },
     "الملف الخاص بي": { screen: DesignerProfile },
-    "صفحة الطلب": { screen: RequestScreen },
-
   },
   { headerMode: "none" }
 );

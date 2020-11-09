c5f8f307cb27a8866a4770cac46a13642027095e
ssssara1998 <59503719+ssssara1998@users.noreply.github.com>
2020-11-09 17:40:15 +0300

    Revert "deleted AllChat"
    This reverts commit a0b1a0cd0742dac883cbef23aa237c21cf782459.

--- components/AllChat.js
+++ components/AllChat.js
@@ -1,132 +1,28 @@
-import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Text, FlatList, TouchableOpacity } from "react-native";
-import Svg, { Defs, G, Path } from "react-native-svg";
+import React, { Component } from "react";
+import { View, StyleSheet } from "react-native";
+//import { Title } from 'react-native-paper';
+//import { AuthContext } from '../navigation/AuthProvider';
+//import FormButton from '../components/FormButton';
 
-import { List, Divider } from "react-native-paper";
-import firebase from "../database/firebase";
-import "firebase/firestore";
-
-
-export default function Display({ navigation }) {
-  const [threads, setThreads] = useState([]);
-
-
-  useEffect(() => {
-    const CurrentID = firebase.auth().currentUser.uid;
-    const unsubscribe =
-      firebase
-        .firestore()
-        .collection("UserID")
-        .doc(CurrentID)
-        .collection("AllChat")
-        // .orderBy('latestMessage.createdAt', 'desc')
-        .onSnapshot(querySnapshot => {
-          const threads = querySnapshot.docs.map(documentSnapshot => {
-            return {
-              _id: documentSnapshot.id,
-              //title: '',
-              ...documentSnapshot.data()
-            };
-          });
-
-          setThreads(threads);
-
-
-        });
-
-    /**
-     * unsubscribe listener
-     */
-    return () => unsubscribe();
-  }, []);
-
-
-
+export default function AllChat() {
+  // function handleButtonPress() {
+  //     if (roomName.length > 0) {
+  //       firestore()
+  //         .collection('THREADS')
+  //         .add({
+  //           name: roomName
+  //           }
+  //         })
+  //         .then(() => {
+  //           navigation.navigate('Home');
+  //         });
+  //     }
+  //   }
   return (
     <View style={styles.container}>
-      <Text
-        style={{
-          fontSize: 25,
-          fontWeight: "700",
-          color: "#4f3c75",
-          alignSelf: "center",
-          position: "absolute",
-          zIndex: 2,
-          top: "6.5%",
-        }}
-      >
-        محادثاتي{" "}
-      </Text>
-      <Svg
-        width={416}
-        height={144}
-        style={{ alignSelf: "center", top: "-3%", position: "relative" }}
-      >
-        <G data-name="Group 7">
-          <G filter="url(#prefix__a)">
-            <Path
-              data-name="Path 117"
-              d="M47 6h322a38 38 0 0138 38v50a38 38 0 01-38 38H47A38 38 0 019 94V44A38 38 0 0147 6z"
-              fill="#ffeed6"
-            />
-          </G>
-          <Path
-            data-name="Icon ionic-ios-arrow-back"
-            d="M53.706 96.783l8.135-8.912a1.793 1.793 0 000-2.379 1.449 1.449 0 00-2.176 0L50.45 95.59a1.8 1.8 0 00-.045 2.323l9.256 10.169a1.451 1.451 0 002.176 0 1.793 1.793 0 000-2.379z"
-            fill="#4f3c75"
-            onPress={() => this.props.navigation.goBack()}
-          />
-          <Path
-            data-name="Icon material-menu"
-            onPress={() => this.props.navigation.toggleDrawer()}
-            d="M336.676 109.883H377V105.4h-40.324zm0-11.2H377V94.2h-40.324zm0-15.683v4.48H377V83z"
-            fill="#4f3c75"
-          />
-        </G>
-      </Svg>
-      <FlatList
-        data={threads}
-        keyExtractor={(item) => item._id}
-        ItemSeparatorComponent={() => <Divider />}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-
-            onPress={() =>
-              navigation.navigate("chat", {
-                thread: item,
-                // to: item.to,
-                // title: title,
-              })
-            }
-          >
-            <List.Item
-              //title={item.title}
-              description="Item description"
-              titleNumberOfLines={1}
-              titleStyle={styles.listTitle}
-              descriptionStyle={styles.listDescription}
-              descriptionNumberOfLines={1}
-            />
-          </TouchableOpacity>
-        )}
-      />
+      <Title>محادثاتي</Title>
+      <Title>جميع المحادثات ستكون متواجدة هنا</Title>
+      {/* <Title>{user.uid}</Title> */}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#fff",
-    flex: 1,
-  },
-  listTitle: {
-    fontSize: 22,
-    textAlign: "right",
-    paddingRight: "5%",
-  },
-  listDescription: {
-    fontSize: 16,
-    textAlign: "right",
-    paddingRight: "5%",
-  },
-});

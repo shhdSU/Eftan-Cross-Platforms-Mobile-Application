a6bbfdcdb8ff17cb48d5213b249e2de0b808ea73
lujain <lujainalmuhaitheef@gmail.com>
2020-12-02 16:50:18 +0300

    تصاميمي

--- components/designerPersonalPortfolio.js
+++ components/designerPersonalPortfolio.js
@@ -156,28 +156,40 @@ export default class designerPersonalPortfolio extends Component {
               flex: 1,
               alignItems: "center",
               shadowOffset: { width: 0.5, height: 0.5 },
-              shadowOpacity: 0.5,
-              shadowRadius: 3,
-              elevation: 5,
-              backgroundColor: "white",
-              marginBottom: 40,
+              shadowColor: "#000",
+              shadowOffset: {
+                width: 0,
+                height: 2,
+              },
+              shadowOpacity: 0.23,
+              shadowRadius: 2.62,
+
+              elevation: 4,
+              // backgroundColor: "white",
+              marginBottom: 30,
               width: 150,
               height: 150,
-              borderRadius: 15,
+              // borderRadius: 15,
             }}
           >
-            <Image
-              style={{
-                width: 140,
-                height: 140,
-                borderColor: "#ccc",
-                borderWidth: 1,
-                top: "3%",
-                borderRadius: 15,
-                alignSelf: "center",
-              }}
-              source={{ uri: element.designUrl }}
-            />
+            <TouchableOpacity
+              onPress={() =>
+                this.props.navigation.navigate("عرض تفاصيل التصميم", {
+                  obj: element,
+                })
+              }
+            >
+              <Image
+                style={{
+                  width: 160,
+                  height: 160,
+                  top: "3%",
+                  borderRadius: 15,
+                  alignSelf: "center",
+                }}
+                source={{ uri: element.designUrl }}
+              />
+            </TouchableOpacity>
             <EvilIcons
               name="trash"
               size={27}
@@ -248,9 +260,10 @@ export default class designerPersonalPortfolio extends Component {
           width={416}
           height={144}
           style={{
-            alignSelf: "center",
-            top: "-9%",
             position: "absolute",
+            alignSelf: "center",
+            top: "-2%",
+            zIndex: 1,
             shadowColor: "#000",
             shadowOffset: {
               width: 0,
@@ -258,28 +271,29 @@ export default class designerPersonalPortfolio extends Component {
             },
             shadowOpacity: 0.32,
             shadowRadius: 5.46,
+
             elevation: 9,
           }}
         >
           <G data-name="Group 7">
-            <G filter="url(#prefix__a)">
+            {/* <G filter="url(#prefix__a)">
               <Path
                 data-name="Path 117"
                 d="M47 6h322a38 38 0 0138 38v50a38 38 0 01-38 38H47A38 38 0 019 94V44A38 38 0 0147 6z"
                 fill="#ffeed6"
               />
-            </G>
-            <Path
+            </G> */}
+            {/* <Path
               data-name="Icon ionic-ios-arrow-back"
               onPressIn={() => this.props.navigation.goBack()}
               d="M53.706 96.783l8.135-8.912a1.793 1.793 0 000-2.379 1.449 1.449 0 00-2.176 0L50.45 95.59a1.8 1.8 0 00-.045 2.323l9.256 10.169a1.451 1.451 0 002.176 0 1.793 1.793 0 000-2.379z"
-              fill="#4f3c75"
-            />
+              fill="#FEB518"
+            /> */}
             <Path
               data-name="Icon material-menu"
               onPress={() => this.props.navigation.toggleDrawer()}
               d="M336.676 109.883H377V105.4h-40.324zm0-11.2H377V94.2h-40.324zm0-15.683v4.48H377V83z"
-              fill="#4f3c75"
+              fill="#FEB518"
             />
           </G>
         </Svg>
@@ -302,9 +316,9 @@ export default class designerPersonalPortfolio extends Component {
             >
               <View
                 style={{
-                  marginTop: 10,
+                  marginTop: 70,
                   paddingLeft: 30,
-                  paddingRight: 30,
+                  paddingRight: 20,
                   justifyContent: "space-between",
                   flexDirection: "row",
                   flexWrap: "wrap",
@@ -333,16 +347,17 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "center",
     backgroundColor: "#fff",
-    top: "5%",
     padding: "1%",
   },
   forText: {
-    position: "absolute",
-    top: "2%",
-    color: "#4F3C75",
     fontSize: 25,
-    textAlign: "center",
     fontWeight: "700",
+    color: "#4f3c75",
+    alignSelf: "center",
+    top: "9%",
+    position: "absolute",
+    zIndex: 2,
+    fontFamily: "Tajawal-Medium",
   },
   trash: {
     top: "9%",
